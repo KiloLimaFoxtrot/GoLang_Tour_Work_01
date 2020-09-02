83891fc4710bd95e173afddaa02b3e6b9600c222
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-09-01 20:44:50 -0700

    goGameTranslateSGFPoint copy for cleaning commit and push update 03

--- GoTour/methods01.go
+++ GoTour/methods01.go
@@ -9,23 +9,23 @@ type VrtxStrct01 struct {
 	X, Y float64
 }
 
+func (v1 VrtxStrct01) Abs() float64 {
+	absVal := math.Sqrt(v1.X*v1.X + v1.Y*v1.Y)
+	return absVal
+}
+
 func main() {
 	fmt.Println()
 	fmt.Println("**** Methods 01:")
-	
+
 	v1 := VrtxStrct01{
 		X: 3,
 		Y: 4,
 	}
-	
+
 	v1AbsVal := v1.Abs()
-	
+
 	fmt.Println()
 	fmt.Println("v1AbsVal: ", v1AbsVal)
-	
-}
 
-func (v1 VrtxStrct01) Abs() float64 {
-	absVal := math.Sqrt(v1.X*v1.X + v1.Y*v1.Y)
-	return absVal
 }
