7c86d4307a7e161bb089f88e53375a2a2a57aad3
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-09-10 03:11:03 -0700

    Update methods01.go

--- GoTour/methods01.go
+++ GoTour/methods01.go
@@ -25,38 +25,57 @@ type VrtxStrct01 struct {
 	X, Y float64
 }
 
+// Declaring a method (ie a function with a receiver argument) on a
+// struct type
 // Function reciever argument: v1 VrtxStrct01
 // Function name: Abs()
 // Function return data type: float64
 // -This type of function, a method,
 // puts the function 'on' the receiving data type's variable instance,
 // see below v1.Abs01()
-func (v1 VrtxStrct01) Abs01() float64 {
-	absVal := math.Sqrt(v1.X*v1.X + v1.Y*v1.Y)
+func (vIn VrtxStrct01) Abs01() float64 {
+	absVal := math.Sqrt(vIn.X*vIn.X + vIn.Y*vIn.Y)
 	return absVal
 }
 
-// The same method without a receiver argument,
-// written as a basic function
+// The same function without a receiver argument,
+func Abs02(vIn VrtxStrct01) float64 {
+	return math.Sqrt(vIn.X*vIn.X + vIn.Y*vIn.Y)
+}
 
-func Abs02(v1 VrtxStrct01) float64 {
-	return math.Sqrt(v1.X*v1.X + v1.Y*v1.Y)
+// Simple get absolute value method
+// declaring method on non-struct type
+type MyFloat01 float64
+
+func (fIn MyFloat01) Abs03() float64 {
+	// This checks to see if fIn is negative, and if so,
+	// returns the negative of that to get the absolute value
+	if fIn < 0 {
+		return float64(-fIn)
+	}
 }
 
 func main() {
 	fmt.Println()
 	fmt.Println("**** Methods 01:")
-
-	v1 := VrtxStrct01{
+	
+	v01 := VrtxStrct01{
 		X: 3,
 		Y: 4,
 	}
-
+	
 	// Here is a function Abs01(
-	// ) 'on' the data type VrtxStrct01 variable instance v1
-	v1AbsVal := v1.Abs01()
-
+	// ) 'on' the data type VrtxStrct01 variable instance v01
+	v1AbsVal := v01.Abs01()
 	fmt.Println()
-	fmt.Println("v1AbsVal: ", v1AbsVal)
-
+	fmt.Println("Method on variable assigned to v1AbsVal: ", v1AbsVal)
+	
+	// condensed version of just above,
+	// removes the variable initialization
+	fmt.Println()
+	fmt.Println("Method on variable - v01.Abs01(): ", v01.Abs01())
+	
+	fmt.Println()
+	fmt.Println("Basic function - Abs02(v01): ", Abs02(v01))
+	
 }
